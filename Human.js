4e3486f9f78f8f6efcc03cf5bd256cba07a10da8
yuichiro <y.y.g.liberty@gmail.com>
2021-01-31 01:37:22 +0900

    Manクラス/Womenクラスsakusei

--- Human.js
+++ Human.js
@@ -1,3 +1,10 @@
-class Human {
-  constructor() {}
+export class Human {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.name}`);
+  }
 }
